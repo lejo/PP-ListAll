2cacada02dc811feb1f7604e5c10469a4130f52d
Lejo Varughese <Lejo.Varughese@gmail.com>
2024-03-04 21:10:17 -0600

    Lejo - Initial version

---
+++ PowerPlatform-List-All-Components-To-CSV.ps1
@@ -0,0 +1,95 @@
+$m365Status = m365 status
+
+if ($m365Status -eq "Logged Out") {
+    # Connection to Microsoft 365
+    m365 login --authType password --userName [USERNAME] --password [PASSWORD]
+}
+
+$environments = m365 pp environment list --asAdmin | ConvertFrom-Json
+
+Write-Host -f Green "Processing $($environments.Count) environments";
+
+$results = @()
+
+$environments | ForEach-Object {
+  Write-Host -f Green "Processing environment: $($_.displayName)"
+  $envId = $($_.name)
+
+  $results += [pscustomobject]@{
+    type        = "environment"
+    location    = $_.location
+    id          = $envId
+    displayName = $_.displayName
+    envId       = $envId
+    lifeCycleId = $_.properties.environmentSku
+    isDefault    = $_.isDefault
+    databasetype = $_.databaseType
+    createdByUpn = $_.properties.createdBy.userPrincipalName
+  }
+
+  $apps = m365 pa app list --environmentName $envId --asAdmin | ConvertFrom-Json -AsHashTable
+
+  if ($apps -ne $null) {
+      Write-Host -f Green "Processing: $($apps.Count) apps"
+
+      $apps | ForEach-Object {
+
+        $name = ""
+        if ($_.name -eq $null) {
+            $name = "<UNKNOWN ID>"
+        } else {
+            $name = $_.name
+        }
+
+        $results += [pscustomobject]@{
+          type              = $_.appType
+          id                = $name
+          displayName       = $_.displayName.Replace(" ","").Replace("/","_").Replace(">","_").Replace("|","_")
+          envId             = $envId
+          lifeCycleId       = $_.properties.lifeCycleId
+          status            = $_.properties.status
+          createdByUpn      = $_.properties.createdBy.userPrincipalName
+          lastDraftVersion  = $_.properties.lastDraftVersion
+        }
+      }
+  } else {
+    Write-Host "No apps found in $envId"
+  }
+
+
+#Read more: https://www.sharepointdiary.com/2021/12/check-null-not-null-empty-in-powershell.html#ixzz8TYHuPhpx
+
+  $flows = m365 flow list --environmentName $envId --asAdmin --includeSolutions | ConvertFrom-Json -AsHashTable
+
+  if ($flows -ne $null) {
+      Write-Host -f Green "Processing: $($flows.Count) flows"
+
+      $flows | ForEach-Object {
+        $createUser = m365 aad user get --id $_.properties.creator.userId | ConvertFrom-Json 
+
+        $name = ""
+        if ($_.name -eq $null) {
+            $name = "<UNKNOWN ID>"
+        } else {
+            $name = $_.name
+        }
+
+        $results += [pscustomobject]@{
+          type          = "flow"
+          id            = $name
+          displayName   = $_.displayName.Replace(" ","").Replace("/","_").Replace(">","_").Replace("|","_")
+          envId         = $envId
+          lifeCycleId   = $_.properties.lifeCycleId
+          status        = $_.properties.state
+          createdByUpn  = $createUser.userPrincipalName
+        }
+      }
+  } else {
+    Write-Host "No flows found in $envId"
+  }
+
+}
+
+$results | Export-Csv -Path .\PowerPlatform-Export-ALL.csv -NoTypeInformation
+
+Write-Host -f Green "***** INVENTORY DISCOVERY COMPLETE *****"
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# PP-ListAll
-Export Power Platform components to CSV
+# COMPONENT INVENTORY
+
+
+
+## Pre-requisites
+
+ 1. PowerShell 7
+ 2. CLI for Microsoft 365
+ 3. Login credentials with at least the Power Platform Admin role
+
+## Instructions
+1. Save the attached PowerShell script.
+*The script only lists PowerApps and Power Automate flows to a local .CSV file for inventory purposes.*
+
+2. Edit the .ps1 file. Specify the username and password.
+*If you prefer to change the authenticate type, please feel free: https://pnp.github.io/cli-microsoft365/cmd/login*
+ 
+3. Save the file.
+
+4. Launch Windows PowerShell
+
+5. Navigate to the location where you saved the .ps1 file
+
+6. Type the name of the .ps1 file (use tab to autocomplete)
+
+7. Execute the script
+
+8. You may receive the following error if this is your first time running PnP. If not, please skip to step 12. 
+>> Error: invalid_grant: AADSTS65001: The user or administrator has not consented to use the application with ID '31359c7f-bd7e-475c-86db-fdb8c937548e' named 'PnP Management Shell'. Send an interactive authorization request for this user and resource. 
+9. If you do, please run the Register-PnPManagementShellAccess cmd as a Global Admin.
+You’ll be prompted to consent to a set of permissions. Consent on behalf of your organization and click on the Accept button.
+
+10. Retry executing the script once consent has been accepted. 
+
+11. The script will display progress as it crawls through your Power Platform environments
+
+12. Once you see **** INVENTORY DISCOVERY COMPLETE **** on the console screen, a .csv file will be generated in the same location (filename: PowerPlatform-Export-ALL.csv)
+
+13. Please send us the file for review. If you run into any issues executing the script, please let us know so we can assist accordingly.
+
+# Storage Capacity Overview
+
+## Pre-requisites
+
+- Login credentials with at least the Power Platform Admin role
+
+## Instructions
+
+1. Navigate to the Power Platform Admin Center: Power Platform admin center (microsoft.com)
+
+2. Navigate to Resources > Capacity
+
+3. Take screenshots of the Dataverse and Microsoft Teams tabs
+
+4. Please send us the screenshots for review
